535cf14543a75c732ddf1b251b473cb24f88c732
ZhangHao <zhanghao@shuto.cn>
2017-12-09 23:44:18 +0800

    first commit

---
+++ src/main/java/com/example/devopsinaction/DevopsinactionApplication.java
@@ -0,0 +1,12 @@
+package com.example.devopsinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsinactionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsinactionApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/devopsinaction/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.devopsinaction.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author 张浩
+ * @version 1.0 2017/12/09.
+ */
+
+@RestController
+public class FirstController {
+    @RequestMapping("/")
+    public String helloDevOps() {
+        return "Hello DevOps!";
+    }
+}
